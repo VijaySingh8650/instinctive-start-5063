da438eb98670a976aa6633aaf027019fe7dd16cf
Vijay Singh Rathore <rathorevijay995@gmail.com>
2022-11-12 08:03:07 +0530

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,12 @@
-
-import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Navbar from './Components/Admin/Navbar/Navbar';
-import SignUp from './Pages/Signup/Signup';
 import AllRoutes from './Routers/AllRoutes';
-// import AdminRouter from './Routers/Admin/AdminRouter';
+
 
 function App() {
   return (
    <>
-   <AllRoutes/>
-      <Routes>
+      <AllRoutes/>
 
-        <Route path="/admin/*" element={<Navbar />}></Route>
-      
-      </Routes>
-      </>
+   </>
   );
 }
-
-export default App;
