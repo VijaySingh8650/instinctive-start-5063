17c3e3e1f870c07ed5bf356ede2804a386934596
Vijay Singh Rathore <rathorevijay995@gmail.com>
2022-11-12 08:05:21 +0530

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,18 +4,27 @@ import './index.css';
 import App from './App';
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from '@chakra-ui/react'
+import { Provider } from 'react-redux';
+import {store} from "./Store/store"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+
     
   
-     <ChakraProvider>
- <BrowserRouter>
+ 
+  
+  
+ <Provider store={store}>  
+  <BrowserRouter>
+    <ChakraProvider>
+
       <App />
-      </BrowserRouter>
-     </ChakraProvider>
-  
-  
+
+    </ChakraProvider>
+  </BrowserRouter>
+  </Provider>   
+
 );
 
 
