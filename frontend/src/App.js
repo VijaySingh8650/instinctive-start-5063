599dba8d8dfd0a9e4f6ff1e19275d406464084e7
Vijay Singh Rathore <rathorevijay995@gmail.com>
2022-11-12 07:57:02 +0530

    APP file Changed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,21 @@
 
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './Pages/Home';
-// import Home from './Pages/Home';
+import Navbar from './Components/Admin/Navbar/Navbar';
+import SignUp from './Pages/Signup/Signup';
+import AllRoutes from './Routers/AllRoutes';
+// import AdminRouter from './Routers/Admin/AdminRouter';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       
-    
-        {/* <img width="20%" src="./logo.png" alt="" /> */}
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-        </Routes>
-      </header>
-    </div>
+   <>
+   <AllRoutes/>
+      <Routes>
+
+        <Route path="/admin/*" element={<Navbar />}></Route>
+      
+      </Routes>
+      </>
   );
 }
 
