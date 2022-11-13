e467ac7f076dd9b8a6297d0db32e0c3609134083
shabaz ali malik <shabazalimalik69@gmail.com>
2022-11-13 13:24:28 +0530

    Wishlist Added

--- frontend/src/Routers/AllRoutes.jsx
+++ frontend/src/Routers/AllRoutes.jsx
@@ -4,10 +4,12 @@ import ForgotPassword from "../Pages/Forgot_Password/ForgotPassword";
 import SignIn from "../Pages/Signin/Signin";
 import SignUp from "../Pages/Signup/Signup";
 import Navbar from "../Components/Admin/Navbar/Navbar";
+import { Wishlist } from "../Pages/Wishlist/Wishlist";
 
 const AllRoutes = () => {
   return (
     <Routes>
+        <Route path="/wishlist" element={<Wishlist />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/signin" element={<SignIn />} />
       <Route path="/forgot_password" element={<ForgotPassword />} />
--- frontend/src/Store/store.js
+++ frontend/src/Store/store.js
@@ -2,10 +2,12 @@ import {combineReducers, legacy_createStore,applyMiddleware,compose} from "redux
 import thunk from "redux-thunk";
 import { authReducer } from "./Auth/auth.reducer";
 import { googleReducer } from "./Google/google.reducer";
+import { wishlistReducer } from "./Wishlist/wishlist.reducer";
 
 const rootReducer = combineReducers({
     auth:authReducer,
     google:googleReducer,
+    wishlist:wishlistReducer,
 })
 const createCompose = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 export const store = legacy_createStore(rootReducer,createCompose(applyMiddleware(thunk)))
